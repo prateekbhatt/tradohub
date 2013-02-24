61b18b9ae0bed100288fcde5122769b3bafcbe61
prateekbhatt <prattbhatt@gmail.com>
2013-02-25 03:36:36 +0530

    added user registration, dashboard template, other small changes

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 
-/**
- * Module dependencies.
- */
+// Module dependencies
 
 var express = require('express')
   , routes = require('./routes')
+  , user_controller = require('./controllers/user_controller')
   , user = require('./routes/user')
-  , auth = require('./routes/auth')
+  , dashboard = require('./routes/dashboard')
   , http = require('http')
   , path = require('path')
   , mongoose = require('mongoose')
   , passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy
-  , bcrypt = require('bcrypt')
-  , SALT_WORK_FACTOR = 10;
+  , less = require('less');
+
+// db settings using mongoose
 
 mongoose.connect('localhost', 'amdavad');
 var db = mongoose.connection;
@@ -22,51 +21,11 @@ db.once('open', function callback() {
   console.log('Connected to DB');
 });
 
-// User Schema
-
-var userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true }, 
-  password: { type: String, required: true }
-});
-
-// Bcrypt middleware
-userSchema.pre('save', function(next) {
-  var user = this;
-
-  if(!user.isModified('password')) return next();
-
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if(err) return next(err);
-
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if(err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-// Password verification
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if(err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-// Seed a user
-
-var User = mongoose.model('User', userSchema);
-var user1 = new User({ email: 'prat', password: 'p' });
-user1.save(function(err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log('user: ' + user.email + ' is saved!');
-  }
-});
+// Create app
 
 var app = express();
+
+// Config settings
 
 app.configure(function(){
   app.set('port', process.env.PORT || 8000);
@@ -78,58 +37,44 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('secretofthedarkhorse'));
   app.use(express.session());
+  // Remember me middleware
+  app.use(user.rememberme);
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
+
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+// Locals (available inside all templates)
+
 app.locals({
   title: 'Amdavad'
 });
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
 
-passport.use(new LocalStrategy(function(email, password, done) {
-  User.findOne({ email: email}, function(err, user) {
-    // console.log('user found \n\n' + user + '\n');
-    if (err) { return done(err); }
-    if (!user) { return done(null, false, { message: 'Unknown user: ' + email }); }
-    user.comparePassword(password, function(err, isMatch) {
-      if (err) { return done(err); }
-      if (isMatch) {
-        // console.log('PASSWORD MATCHED \n\n');
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Invalid password' });
-      }
-    });
-  });
-}));
-
+// Routes
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/account', auth.ensureAuthenticated, user.account);
-app.get('/login', auth.login);
-app.get('/logout', auth.logout);
-
+app.get('/register', user.register);
+app.get('/login', user.login);
+app.get('/dashboard', user.ensureAuthenticated, dashboard.index);
+app.post('/register', user.registerPost);
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('/dashboard');
   });
+app.get('/logout', user.logout);
+
+// Start server
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,67 @@
+var mongoose = require('mongoose')
+    , user_controller = require('../controllers/user_controller')
+    , userSchema = user_controller.userSchema;
 
-/*
- * GET users listing.
- */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+// Remember Me middleware
+exports.rememberme = function (req, res, next) {
+  if ( req.method == 'POST' && req.url == '/login' ) {
+    if ( req.body.rememberme ) {
+      req.session.cookie.maxAge = 2592000000; // 30*24*60*60*1000 Rememeber 'me' for 30 days
+      // console.log('INSIDE rememberme: '+req+'\n\n');
+      // for (var i in req.session.cookie) {
+      //   console.log(i+' : '+req.session.cookie[i]);
+      // }
+    } else {
+      req.session.cookie.expires = false;
+    }
+  }
+  next();
 };
 
-exports.account = function(req, res){
-	res.render('account', { 'user': req.user });
+exports.login = function (req, res) {
+  res.render('login', { 'user': req.user });
+};
+
+exports.logout = function (req, res) {
+  req.logout();
+  res.redirect('/');
+};
+
+exports.register = function (req, res) {
+  res.render('register', { 'user': req.user });
+};
+
+exports.registerPost = function (req, res, next) {
+  console.log('INSIDE REGISTER POST');
+  for (var i in req.body) { console.log(i+' : '+req.body['email']); }
+  if (req.body['email'] && req.body['password'] && req.body['confirmPassword']) {
+    console.log('PASSWORD AND CONFIRM_PASSWORD MATCH\n');
+    var user = new user_controller.userModel({ email: req.body['email'], password: req.body['password'] });
+    user.save(function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect('/register');
+      } else {
+        console.log('user: ' + user.email + ' is saved!');
+        res.redirect('/login');
+      }
+    });
+  } else {
+    console.log('ERROR DURING REGISTRATION');
+    res.redirect('/register');
+  }
+};
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+exports.ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
 };
